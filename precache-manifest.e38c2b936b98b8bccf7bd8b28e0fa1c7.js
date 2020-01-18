2a84c4bae193c2c2aef6179d25c0af0c21dcb5c5
Matt Mapplebeck <mmapplebeck@flatiron.com>
2020-01-17 21:29:42 -0500

    Updates

---
+++ precache-manifest.e38c2b936b98b8bccf7bd8b28e0fa1c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f52b526d2fd9cf1023b38f00dc741a",
+    "url": "/dictionary-manager/index.html"
+  },
+  {
+    "revision": "eae3eb114697b1307355",
+    "url": "/dictionary-manager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af32d7ff16da50b4cb36",
+    "url": "/dictionary-manager/static/js/2.0a40ba2b.chunk.js"
+  },
+  {
+    "revision": "764ee7d7b2b37ea6ad2fc352f6f4060b",
+    "url": "/dictionary-manager/static/js/2.0a40ba2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eae3eb114697b1307355",
+    "url": "/dictionary-manager/static/js/main.151c7d5f.chunk.js"
+  },
+  {
+    "revision": "61ae9075cf8df2f1b634",
+    "url": "/dictionary-manager/static/js/runtime-main.1a9e8b2d.js"
+  }
+]);
